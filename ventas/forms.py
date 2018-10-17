d995f3fa17d26b9b09dd3f3cdeb3c970872e11a9
Joseph Student <24597827@e.ne.udo.edu.ve>
2018-10-17 02:58:02 -0400

    Agregado crud de cliente

--- ventas/forms.py
+++ ventas/forms.py
@@ -46,6 +46,32 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = Clientes
         fields = '__all__'
+        widgets = {
+            'denomcliente': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Denominación del Cliente'
+                }
+            ),
+            'desccliente': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Descripción del Cliente'
+                }
+            ),
+            'fecharegistro': forms.DateTimeInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'DD/MM/YYYY HH:MM:SS'
+                }
+            ),
+            'clienteactivo': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Cliente Activo'
+                }
+            )
+        }
 
 
 class DetVentasForm(forms.ModelForm):
